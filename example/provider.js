0207f8f4814f82d27b6090fccf80b10f2bf5aff4
Anisetus Elly Efendi <3lly3fendi@gmail.com>
2017-08-17 13:50:22 +0700

    Add example provider constant

---
+++ example/provider.js
@@ -0,0 +1,17 @@
+/**
+ * example-angularjs
+ * https://github.com/3lXample/example-angularjs
+ *
+ * Copyright (c) 2017 3lXample (https://github.com/3lXample)
+ * Licensed under the MIT license
+ */
+(function() {
+  'use strict';
+
+  var name   = '3XApp-example';
+  var module = angular.module(name);
+
+  // Add Constants
+  module.constant('exampleConstant', 'built by Anisetus Elly Efendi');
+
+})();
