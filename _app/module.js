4cfd285e1ae7ce7862347bb6f316c504594cf83b
Anisetus Elly Efendi <3lly3fendi@gmail.com>
2017-08-17 13:30:03 +0700

    Add AngularJS Skeleton

---
+++ _app/module.js
@@ -0,0 +1,15 @@
+/**
+ * example-angularjs
+ * https://github.com/3lXample/example-angularjs
+ *
+ * Copyright (c) 2017 3lXample (https://github.com/3lXample)
+ * Licensed under the MIT license
+ */
+(function() {
+  'use strict';
+
+  var name         = '3XApp';
+  var dependencies = [];
+  var module       = angular.module(name, dependencies);
+
+})();
---
+++ _app/provider.js
@@ -0,0 +1,14 @@
+/**
+ * example-angularjs
+ * https://github.com/3lXample/example-angularjs
+ *
+ * Copyright (c) 2017 3lXample (https://github.com/3lXample)
+ * Licensed under the MIT license
+ */
+(function() {
+  'use strict';
+
+  var name   = '3XApp';
+  var module = angular.module(name);
+
+})();
